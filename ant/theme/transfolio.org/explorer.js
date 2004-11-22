212cb7ad08cbbe55ab0ea821dd15e020d996c853
glorieux <glorieux>
2004-11-22 11:31:16 +0000

    Deep reorganization of what was called "skin", to be now named as "theme" (more multinational name). Contains the default template for layout of html pages.

---
+++ ant/theme/transfolio.org/explorer.js
@@ -0,0 +1,227 @@
+/*
+Explorer methods
+
+  var height;
+  if (document.body && document.body.offsetHeight) height=document.body.offsetHeight;
+  else if (document.body && document.body.clientHeight) height=document.body.clientHeight;
+  else if (window.innerHeight) height= window.innerHeight;
+
+*/
+
+/*
+TODO, find a way to put this in frame
+*/
+
+function explorerInit() {
+  if (!document.getElementById) return false; 
+  var dir=document.getElementById("dir");
+  if (!dir) return false;
+  
+  dir.links=dir.getElementsByTagName('A');
+  if(dir.links.length == 0) dir.links=dir.getElementsByTagName('a');
+  var i=0;
+  // set index of 
+  while(dir.links[i]) {
+    dir.links[i].index=i;
+    dir.links[i].onkeydown=explorerKeyDown;
+    dir.links[i].onmouseover=explorerMouseOver;
+    dir.links[i].onclick=explorerLinkClick;
+    i++;
+  }
+}
+
+function explorerLinkClick() {
+  // in case of IE ()
+  if (document.all ) event.cancelBubble = true;
+}
+
+function expand(button, action) {
+  if (!document.getElementById) return false; 
+  if (!button || !button.id) return false;
+  id=button.id;
+  var o=document.getElementById(id+'-');
+  if (!o || !o.style) return false;
+  if (!action) if (o.style.display == "") action="close"
+  else if (!action) action="open";
+  if (action == "open") o.style.display="";
+  else if (action == "close") o.style.display="none";
+  else o.style.display=(o.style.display == 'none')?'':'none';
+  if(button.className) button.className=action;
+  // in case of IE 
+  if (document.all ) event.cancelBubble = true;
+  return false;
+}
+
+
+
+
+function explorerMouseOver(e, o) {
+  if (!o) o=this;
+  o.focus();
+}
+
+function goLang(select) {
+  var s=new String(window.location); 
+  var lang=select.options[select.selectedIndex].text;
+  var sharp="";
+  var query="";
+  if (s.indexOf('#') != -1) {
+    sharp=s.substring(s.indexOf('#'));
+    s=s.substring(0,s.indexOf('#')) ;
+  }
+  if (s.indexOf('?') != -1) {
+    query=s.substring(s.indexOf('?'));
+    s=s.substring(0,s.indexOf('?')) ;
+  }
+  if( s.lastIndexOf('.') - s.search(/_..\./) == 3) s=s.replace(/_..\./, '.');
+  s=s.substring(0, s.lastIndexOf('.')) + '_'+lang + s.substring(s.lastIndexOf('.')); 
+  window.location=s+query+sharp;
+}
+
+function getKey(e) {
+  if (document.all) e = window.event;
+  if(!e)return;
+  if (e.keyCode) return e.keyCode;
+  else if (e.which) return e.which;
+  else return;
+}
+
+function explorerKeyDown(e, o) {
+  if(!o) o=this;
+  key=getKey(e);
+  if (!document.getElementById) return false; 
+  var dir=document.getElementById("dir");
+  if (!dir || !dir.links) return false;
+  // previous
+  if (key==38) {
+    if (o.index == 0) return;
+    previous=dir.links[o.index - 1];
+    while (previous && previous.index > 0 && !isVisible(previous)) {
+      previous=dir.links[previous.index - 1];
+    }
+    if (!previous || !isVisible(previous)) return;
+    previous.focus();
+    return false;
+  }
+  // next
+  else if (key==40) {
+    if (o.index == dir.links.length - 1) return;
+    next=dir.links[o.index + 1];
+    while (next && !isVisible(next) && next.index < dir.links.length - 2 ) {
+      next=dir.links[next.index + 1];
+    }
+    if (!next || !isVisible(next)) return;
+    next.focus();
+    return false;
+  }
+  // open
+  else if (key==39) {
+    if (o.parentNode) expand(o.parentNode, "open"); 
+  }
+  // close
+  else if (key==37) {
+    if (o.parentNode) expand(o.parentNode, "close"); 
+  }
+  // page up
+  else if(key==33) {
+    diff=10;
+    if (o.index - diff < 0) return;
+    previous=dir.links[o.index - diff];
+    while (previous && previous.index > 0 && !isVisible(previous)) {
+      previous=dir.links[previous.index - 1];
+    }
+    if (!previous || !isVisible(previous)) return;
+    previous.focus();
+    return false;
+  }
+  // page down
+  else if(key==34) {
+    diff=10;
+    if (o.index + diff > dir.links.length - 1) return;
+    next=dir.links[o.index + diff];
+    while (next && !isVisible(next) && next.index < dir.links.length - 2 ) {
+      next=dir.links[next.index + 1];
+    }
+    if (!next || !isVisible(next)) return;
+    next.focus();
+    return false;
+  }
+  
+}
+
+function isVisible (o) {
+  while (o && o.style) {
+    if (o.style.display == "none") return false;
+    o=o.parentNode;
+  }
+  return true;
+}
+
+/*
+get properties of an object
+for debug
+*/
+function props(o) {
+   var result = ""
+   var a=new Array();
+   var i=0
+   for (var prop in o) {
+      a[i]= prop  + "\t"; // + " = " + o[prop]
+      i++;
+   }
+   a.sort();
+   return a;
+}
+
+/*
+      <frameset cols="25%,*" frameborder="1" border="1" framespacing="2">
+         <frame src="./toc.xhtml" name="navigation" frameborder="yes" scrolling="auto"/>
+         <frame src="./index" name="article" frameborder="yes" scrolling="auto"/>
+      </frameset>
+
+A try to write a frameset in XSL transformation, don't work
+
+var frameset = document.createElement('frameset');
+frameset.setAttribute("cols", "25%,*");
+frameset.setAttribute("frameborder", "1");
+frameset.setAttribute("border", "1");
+frameset.setAttribute("framespacing", "2");
+
+// left frame
+var append=document.createElement('frame');
+append.setAttribute("src", "");
+append.setAttribute("name", "left");
+append.setAttribute("scrolling", "auto");
+append.setAttribute("frameborder", "yes");
+frameset.appendChild(append);
+
+// right frame
+var append=document.createElement('frame');
+append.setAttribute("src", "index.xhtml");
+append.setAttribute("name", "right");
+append.setAttribute("scrolling", "auto");
+append.setAttribute("frameborder", "yes");
+frameset.appendChild(append);
+
+var html=document.documentElement;
+var body=html.getElementsByTagName('body')[0];
+
+while(body.firstChild) {
+  body.removeChild(body.firstChild);
+}
+var h1=document.createElement('H1');
+h1.appendData("Coucou");
+body.appendChild(h1);
+
+while(body.firstChild) {
+  body.removeChild(body.firstChild);
+}
+
+while(html.firstChild) {
+  html.removeChild(html.firstChild);
+}
+
+html.appendChild( frameset);
+*/
+
+
---
+++ ant/theme/transfolio.org/html.js
@@ -0,0 +1,105 @@
+
+// set functions here will unplug the ones declared in the doc
+// window.onload=init;
+/*
+var bugRiddenCrashPronePieceOfJunk = (
+	navigator.userAgent.indexOf('MSIE 5') != -1
+	&&
+	navigator.userAgent.indexOf('Mac') != -1
+);
+*/
+/*
+get properties of an object
+for debug
+*/
+function props(o) {
+   var result = ""
+   var a=new Array();
+   var i=0
+   for (var prop in o) {
+      a[i]= prop  + "\t"; // + " = " + o[prop]
+      i++;
+   }
+   a.sort();
+   return a;
+}
+
+
+/*
+Simulate the css position:fixed property for IE
+Should be deprecated
+
+assume we have a header, side, and footer object
+in our case, div with id
+*/
+
+function fixed() {
+	if (!document.all) return;
+    if (document.side) side.style.pixelTop=document.body.scrollTop+100; 
+    if (!document.footer) return;
+    footer.style.display='none'; 
+	footer.style.bottom='0px'; 
+	footer.style.display=''; 
+}
+
+/*
+Fit character size to width of screen
+Prepare the fixed() simulate for IE
+Means, set position:absolute to the fixed elements
+*/
+
+function init(width) {
+    if (!width) width=80;
+	fit(width);
+	if (!document.all) return;
+// 	if (header.style) header.style.position='absolute';
+	if (document.side) if (document.side.style) side.style.position='absolute';
+	if (document.footer) if (document.footer.style) footer.style.position='absolute';
+	window.onscroll=fixed;
+}
+
+/*
+Maintain a fixed number of character on the width of the window
+
+*/
+
+// normal pixel height
+var fontSizeNormal=16;
+var widthDefault=80;
+if ( document.getElementById && !document.all ) var fontSizeNormal=12; // may be mozilla
+
+function fit(ex) {
+
+  // fit char size  
+  var width;
+  if (false);
+  else if (document.body && document.body.offsetWidth) width=document.body.offsetWidth;
+  else if (document.body && document.body.clientWidth) width=document.body.clientWidth;
+  else if (window.innerWidth) width= window.innerWidth;
+  else return false;
+
+
+  if (!ex) var ex=widthDefault;
+  // default ratio between ex and em 
+  fontSizeFit=Math.floor((width/ex)*1.2);
+  fontSizeFit=(fontSizeFit < 10)?10:fontSizeFit;
+  //ratio=Math.round ( fontSizeFit / fontSizeNormal  * 100);
+  // set a minimum ratio under which the page may be unreadable
+  //ratio=(ratio < 50)?50:ratio;
+  // don't work
+  // window.fontSizeNormal=25;
+
+  if(document && document.body && document.body.style) document.body.style.fontSize=fontSizeFit+"px";
+  else if (!document.getElementById) return true;
+  // work around for moz in case of XML transformed
+  else if(document.getElementById("body")) { 
+    o=document.getElementById("body");
+    if(!o.style) return false;
+    o.style.fontSize=fontSizeFit+"px";
+  }
+
+  
+  // alert(document.getElementById("body"));
+}
+
+
